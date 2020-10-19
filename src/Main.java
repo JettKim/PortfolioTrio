84361d8327638926ae2df975d5bf7622f32d27b2
Jett Kim <jettkim1@gmail.com>
2020-10-19 09:02:55 -0700

    CalculatorCommitUpdate3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import application.Home;
 
 public class Main {
-
     public static void main(String[] args){
         Portfolio.main(null);
     }
--- src/Portfolio.java
+++ src/Portfolio.java
@@ -1,7 +1,7 @@
-import calculatorfolder.CalculatorUI;
 import application.Home;
+import calculatorfolder.CalculatorUI;
 
-import javax.swing.JFrame;
+import  javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -21,65 +21,9 @@ public class Portfolio extends JFrame{
 
     }
 
+
+    JButton game_button = new JButton("Game");
     JButton calculator_button = new JButton("calculator");
-
-    int r = 100;
-    int g = 100;
-    int b = 100;
-    Boolean rpeaked = false;
-    Boolean gpeaked = false;
-    Boolean bpeaked = false;
-
-    Random rand = new Random();
-
-    protected Timer timer = new Timer(20, new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println(Integer.toString(r) + ", " + Integer.toString(g) + ", " + Integer.toString(b));
-
-            if (!rpeaked){
-                r += rand.nextInt(5);
-            }else{
-                r -= rand.nextInt(5);
-            }
-            if(r >= 255){
-                rpeaked=true;
-                r = 255;
-            }else if(r <= 0){
-                rpeaked = false;
-                r = 0;
-            }
-
-
-            if (!gpeaked){
-                g += rand.nextInt(5);
-            }else{
-                g -= rand.nextInt(5);
-            }
-            if(g >= 255){
-                gpeaked=true;
-                g=255;
-            }else if(g <= 0){
-                gpeaked = false;
-                g=0;
-            }
-
-            if (!bpeaked){
-                b += rand.nextInt(5);
-            }else{
-                b -= rand.nextInt(5);
-            }
-            if(b >= 255){
-                bpeaked=true;
-                b = 255;
-            }else if(b <= 0){
-                bpeaked = false;
-                b = 0;
-            }
-
-            getContentPane().setBackground(new Color(r, g, b));
-        }
-    });
 
     public Portfolio(){
 
@@ -87,15 +31,19 @@ public class Portfolio extends JFrame{
         setPreferredSize(new Dimension(500,500));
         setLayout(new FlowLayout());
 
+        game_button.addActionListener(e -> {
+            System.out.println("aimboost");
+            Home.main(null);
+        });
+        add(game_button);
+
+
         calculator_button.addActionListener(e -> {
             System.out.println("calculator");
             CalculatorUI.main(null);
         });
         add(calculator_button);
 
-
-        timer.start();
         pack();
-
     }
 }
